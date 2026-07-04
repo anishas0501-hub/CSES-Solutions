99f19caeb6475e9c4dff01f8553eb06855c45b70
Anisha Sadhukhan <anisha.s0501@gmail.com>
2026-07-04 09:44:31 +0530

    better solution Playlist

--- Sorting and Searching/Playlist.cpp
+++ Sorting and Searching/Playlist.cpp
@@ -1,20 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n;
     cin >> n;
-    vector<int> id(n);
-    for(int i = 0; i < n; i++) cin >> id[i];
-    unordered_set<int> st;
-    int l = 0, ans = 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    map<int,int> last;
+
+    int l = 0;
+    int ans = 0;
+
     for(int r = 0; r < n; r++) {
-        while(st.count(id[r])) {
-            st.erase(id[l]);
-            l++;
-        }
-        st.insert(id[r]);
+        if(last.count(a[r]))
+            l = max(l, last[a[r]] + 1);
+
+        last[a[r]] = r;
         ans = max(ans, r - l + 1);
     }
-    cout << ans; 
-}
+    cout << ans;
+} 
